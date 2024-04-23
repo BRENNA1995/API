7784ff13fe2a3d503d2082e50b02435197a0191f
brenna.farace <brenna.farace@maxinst.com>
2024-04-23 20:52:58 -0300

    Backend Crud

--- backend/src/routes/routerAPI.js
+++ backend/src/routes/routerAPI.js
@@ -1,66 +1,53 @@
 const express = require('express');
 const routerAPI = express.Router();
-import axios from 'axios'; 
-const axios = require("axios"); 
-// processa o corpo da requisição e insere os dados recebidos 
-// como atributos de req.body
 routerAPI.use(express.json());
 routerAPI.use(express.urlencoded({ extended: true }))
 
-//INSERT 
-axios.post('https://api.themoviedb.org/3/movie', userData)
-    .then(response => {
-    console.log(response.data);
+routerAPI.get ('/comentario/:id', (req, res) => {
+    let id=req.params.id;
+    knex('comentario').where('id','id')
+    .then((dados)=> {
+        res.json(dados);
+    })
+    .catch((err) => {
+    res.json({ message:`Erro ao inserir os produtos: ${err.message}`});
+    })
 })
-    .catch(error => {
-    console.error(error);
-});
 
-axios.get('https://your-api-url.com/users')
-    .then(response => {
-    console.log(response.data);
-})
-    .catch(error => {
-    console.error(error);
-});
+routerAPI.post('/comentario', (req, res) => {
+    console.log (req.body);
     
-      
-axios.put('https://your-api-url.com/users/1', updatedUserData)
-    .then(response => {
-    console.log(response.data);
-})
-    .catch(error => {
-    console.error(error);
-});
-        
-axios.delete('https://your-api-url.com/users/1')
-    .then(response => {
-    console.log(response.data);
-})
-    .catch(error => {
-    console.error(error);
-});
-  
+    knex('comentario')
+        .insert(req.body,['id'])
+        .then((dados)=> {
+            if (dados.length > 0){
+                const id=dados[0].id;
+                res.status(201).json( { 
+                    message: 'Produto adicionado com sucesso',
+                    data: { id} } ); 
+            }
+        })
+        .catch((err) => {
+        res.json({ message:`Erro ao inserir os produtos: ${err.message}`});
+        })
+
+}) 
+
+routerAPI.delete('/comentario/:id', (req, res)=> {
+    knex('comentario').where('id', req.params.id)
+        .del()
+        .then((dados)=>  {
+                knex.select()
+                .from('comentario')
+                .then((dados)=> {
+                    res.json(dados);
+                })
+        })
+        .catch((err) => {
+            res.json({ message:`Erro ao excluir o produto: ${err.message}`});
+        })
+    })
 
 
-
-// routerAPI.get ('/comentario', (_req, res) => {
-//     res.json (comentario);
-// })
-
-// routerAPI.get ('/comentario/:id', (req, res) => {
-//     let comentario = comentario.find (p => p.id == req.params.id);
-//     res.json (comentario);
-// })
-
-// routerAPI.post('/comentario', (req, res) => {
-//     console.log (req.body);
-//     req.body.id = comentario.length + 1;
-//     comentario.push (req.body);+
-
-//     res.status(201).json( { 
-//         message: 'Produto adicionado com sucesso',
-//         data: { id: req.body.id } });  
-// })
 
 module.exports = routerAPI;
