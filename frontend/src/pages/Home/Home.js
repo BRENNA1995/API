6838c06986872a02c08acf0fd21ee8f87e605cff
brenna.farace <brenna.farace@maxinst.com>
2024-05-22 21:36:17 -0300

    Responsividade do home.js

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState } from "react";
 import { MoviesList } from "../../components/MoviesList/MoviesList";
 import { SearchMovies } from "../../components/SearchMovies/SearchMovies";
 import { useGetMovies } from "../../hooks/useGetMovies";
@@ -6,20 +6,22 @@ import { useSearchMovieByTitle } from "../../hooks/useSearchMovieByTitle";
 // import styles from "./Home.module.css";
 
 export function Home() {
-	const [searchQuery, setSearchQuery] = useState("");
-	const movies = useGetMovies();
-	const searchResults = useSearchMovieByTitle(searchQuery);
+  const [searchQuery, setSearchQuery] = useState("");
+  const movies = useGetMovies();
+  const searchResults = useSearchMovieByTitle(searchQuery);
 
-	function handleOnSearch(movieTitle) {
-		setSearchQuery(movieTitle);
-	}
+  function handleOnSearch(movieTitle) {
+    setSearchQuery(movieTitle);
+  }
 
-	return (
-		<article>
-			<body className="container">			
-				<SearchMovies onSearch={handleOnSearch} />
-				<MoviesList movies={searchResults.length > 0 ? searchResults : movies} />
-			</body>
-		</article>
-	);
+  return (
+    <article>
+      <body className="container">
+        <SearchMovies onSearch={handleOnSearch} />
+        <MoviesList
+          movies={searchResults.length > 0 ? searchResults : movies}
+        />
+      </body>
+    </article>
+  );
 }
