b857f26589788fa636f80278cd6d7c0bc356d908
BRENNA1995 <brennafarace@gmail.com>
2024-07-10 18:20:19 -0300

    Teste4

--- frontend/src/config/http.js
+++ frontend/src/config/http.js
@@ -3,7 +3,7 @@ import { env } from "./env";
 const defaultEnv = 'production';
 
 export const api = axios.create({
-    baseURL: process.env.HOSTNAME_API,
+    baseURL: 'api-backend-red.vercel.app',
     headers: {
         "Content-Type": "application/json",
       }, 
