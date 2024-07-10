cdf0c4f2fc39e219f5e65ee00d0badcc76bdefa8
BRENNA1995 <brennafarace@gmail.com>
2024-07-10 19:38:07 -0300

    ok

--- frontend/src/components/UsuariosList/UsuariosList.js
+++ frontend/src/components/UsuariosList/UsuariosList.js
@@ -3,7 +3,7 @@ import { UsuarioItem } from "../UsuarioItem/UsuarioItem"
 export function UsuarioList({ usuarios }) {
 	return (
 		<section className="with-header">
-			<ul className="container collection">{usuarios.length > 0 ?? usuarios.map((usuario, index) => <UsuarioItem key={index} usuario={usuario} />)}</ul>
+			<ul className="container collection">{usuarios.map((usuario, index) => <UsuarioItem key={index} usuario={usuario} />)}</ul>
 		</section>
 	)
 }
